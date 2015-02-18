869eddcbb1e2a84ab40fa95299ff57bb498a16dc
dnacombo <maximilien.chaumon@gmail.com>
2015-02-18 16:06:01 +0100

    fix when only one comp in last pop_selectcomp call

--- eeg_SASICA.m
+++ eeg_SASICA.m
@@ -730,7 +730,11 @@ COLREJ = '[1 0.6 0.6]';
 COLACC = '[0.75 1 0.75]';
 buttons = findobj(hfig,'-regexp','tag','^comp\d{1,3}$');
 buttonnums = regexp(get(buttons,'tag'),'comp(\d{1,3})','tokens');
-buttonnums = cellfun(@(x)(str2num(x{1}{1})),buttonnums);
+if numel(buttonnums)>1
+    buttonnums = cellfun(@(x)(str2num(x{1}{1})),buttonnums);
+else
+    buttonnums = str2num(buttonnums{1}{1});
+end
 for i = 1:numel(buttonnums)
     hcmenu = uicontextmenu;
     
