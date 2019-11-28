c7f410420df73e18348b59ce403043840db1eaee
Arnaud Delorme <arnodelorme@gmail.com>
2019-11-28 12:29:22 -0600

    Update eegplugin_SASICA.m
    The "Remove component" menu is gone.

--- eegplugin_SASICA.m
+++ eegplugin_SASICA.m
@@ -1,14 +1,11 @@
 function vers = eegplugin_SASICA(fig,try_strings,catch_strings)
 
-vers = 'SASICA_1.3.5';
+vers = 'SASICA_1.3.6';
 
 if nargin == 0
     return
 end
 
 toolsmenu = findobj(fig,'Tag','tools');
-abovemenupos = get(findobj(toolsmenu,'Label','Remove components'),'position');
 
-
-uimenu( toolsmenu,'position',abovemenupos+1,'separator','on', 'label', 'SASICA', 'callback',...
-    [ 'SASICA(EEG);' ]); 
+uimenu( toolsmenu,'separator','on', 'label', 'SASICA', 'callback',[ 'SASICA(EEG);' ]); 
