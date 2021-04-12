08989e9ab8f7be0d172b76fa059008407f8cf14c
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2021-04-12 12:17:39 +0200

    update messages when no/bad data loaded
    (cherry picked from commit f042bb0e72049d3bfd95edcf81c88b87b8ec60ec)
    
    # Conflicts:
    #	eegplugin_SASICA.m

--- eegplugin_SASICA.m
+++ eegplugin_SASICA.m
@@ -1,14 +1,11 @@
 function vers = eegplugin_SASICA(fig,try_strings,catch_strings)
 
-vers = 'SASICA_1.3.5';
+vers = 'SASICA_1.3.7';
 
 if nargin == 0
     return
 end
 
 toolsmenu = findobj(fig,'Tag','tools');
-abovemenupos = get(findobj(toolsmenu,'Label','Remove components'),'position');
 
-
-uimenu( toolsmenu,'position',abovemenupos+1,'separator','on', 'label', 'SASICA', 'callback',...
-    [ 'SASICA(EEG);' ]); 
+u = uimenu( toolsmenu,'separator','on', 'label', 'SASICA', 'callback',[ 'SASICA;' ], 'userdata', 'startup:off;study:on;chanloc:on;ica:on'); 
